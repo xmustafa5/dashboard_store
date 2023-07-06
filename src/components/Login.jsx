e64c9cd14feb2c1285bf38e0fdfbf4f27e195826
mustafa <musmoh73@gmail.com>
2023-07-06 19:59:57 +0300

    login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,31 @@
 import { Card,Form,Button} from "react-bootstrap";
 import { Link } from "react-router-dom";
 
+
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+  const { signup } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError("Passwords do not match");
+    }
+    try {
+      setError("");
+      setLoading(true);
+      await signup(emailRef.current.value, passwordRef.current.value);
+      navigate("/");
+    } catch {
+      setError("Failed to create an account");
+    }
+    setLoading(false);
+  }
+
+
 const Login = () => {
     return(
         <>
@@ -8,7 +33,7 @@ const Login = () => {
         <Card>
         <Card.Body>
             <h2 className="text-center mb-4">Signup</h2>
-            <Form>
+            <Form onSubmit={handlesubmit}>
                 <Form.Group>
                     <Form.Label htmlFor="email">Email</Form.Label>
                     <Form.Control type="email" id="email" />
