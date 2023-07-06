b63a24fd0c54bd02368d9bf0135b70b99343e2c1
mustafa <musmoh73@gmail.com>
2023-07-06 21:43:32 +0300

    update login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,59 +1,64 @@
-import { Card,Form,Button, Alert} from "react-bootstrap";
-import { Link, useNavigate } from "react-router-dom";
+import { React, useRef, useState } from "react";
 import { useAuth } from "../context/AuthContext";
-import { useRef, useState } from "react";
+import { Form, Button, Card, Alert } from "react-bootstrap";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 
-const Login = () => {
-    const {login} = useAuth
-    const emailRef = useRef();
+export default function Login() {
+  const emailRef = useRef();
   const passwordRef = useRef();
+  const { login } = useAuth();
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
-   const navigate = useNavigate();
-
-  async function handlesubmit(e) {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const redirectPath = location.state?.path || "/";
+  async function handleSubmit(e) {
     e.preventDefault();
+
     try {
       setError("");
       setLoading(true);
       await login(emailRef.current.value, passwordRef.current.value);
-       navigate("/");
+      navigate('/');
     } catch {
       setError("Failed to log in");
     }
+
     setLoading(false);
   }
-    return(
-        <>
-        
-        <Card>
-        <Card.Body>
-            <h2 className="text-center mb-4">log in</h2>
-            {error && <Alert variant="danger">{error}</Alert>}
-            <Form onSubmit={handlesubmit}>
-                <Form.Group>
-                    <Form.Label htmlFor="email">Email</Form.Label>
-                    <Form.Control type="email" id="email" ref={emailRef} />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label htmlFor="password">Password</Form.Label>
-                    <Form.Control type="password" id="password" ref={passwordRef} />
-                </Form.Group>
-             
-                <Button variant="primary" type="submit" disabled={loading}>
-                Log in
-      </Button>
-            </Form>
-            <div className="w-100 text-center mt-3">
-            <Link to="/ForgotPassword">forgot password</Link>
-            </div>
-        </Card.Body>
-        </Card>
-        <div className="w-100 text-center mt-2">
-        need an account <Link to="/singup">signup</Link>
-        </div>
-        </>
-    )
-}
 
-export default Login;
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Log In</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group>
+              <Form.Label htmlFor="email">Email</Form.Label>
+              <Form.Control id="email" type="email" ref={emailRef} required />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label htmlFor="password">Password</Form.Label>
+              <Form.Control
+                id="password"
+                type="password"
+                ref={passwordRef}
+                required
+              />
+            </Form.Group>
+            <Button disabled={loading} className="w-100 mt-3" type="submit">
+              Log In
+            </Button>
+          </Form>
+          <div className="w-100 text-center mt-3">
+            <Link to="/forgot-password">Forgot Password?</Link>
+          </div>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        Need an account? <Link to="/signup">Sign Up</Link>
+      </div>
+    </>
+  );
+}
