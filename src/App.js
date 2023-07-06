8c7a43fdc9ca4060911498bf56b4acd07b41821e
mustafa <musmoh73@gmail.com>
2023-07-06 17:43:37 +0300

    add routers and signup page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-
-function App() {
+import { Container } from 'react-bootstrap';
+import { Route, Routes } from 'react-router-dom';
+// import {Container} from "react-bootstrap"
+const App =() =>{
   return (
-    <div className="App">
-    app
-    </div>
+     <Container className='d-flex align-item-center justify-content-center ' style={{minHeight:"100vh"}}>
+sss
+<div className='w-100' style={{maxWidth:"400px"}}>
+<Routes>
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/update-profile' element={<UpdateProfile />} />
+          <Route path='/' element={<Dashboard />} />
+        
+
+        </Routes>
+</div>
+    </Container>
   );
 }
 
