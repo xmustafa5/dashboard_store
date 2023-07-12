95c17c38f094a7c2b28215971801cdc6985b7aaf
mustafa <musmoh73@gmail.com>
2023-07-12 21:56:01 +0300

    delete database

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,42 +1,37 @@
-import { React, useRef, useState } from "react";
-import { Form, Button, Card, Alert } from "react-bootstrap";
-import { Link, } from "react-router-dom";
+import { useState } from "react"
 
-export default function Login() {
- 
+function Login() {
 
-  return (
-    <>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Log In</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <Form onSubmit={handleSubmit}>
-            <Form.Group>
-              <Form.Label htmlFor="email">Email</Form.Label>
-              <Form.Control id="email" type="text" ref={emailRef} required />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label htmlFor="password">Password</Form.Label>
-              <Form.Control
-                id="password"
-                type="password"
-                ref={passwordRef}
-                required
-              />
-            </Form.Group>
-            <Button disabled={loading} className="w-100 mt-3" type="submit">
-              Log In
-            </Button>
-          </Form>
-          <div className="w-100 text-center mt-3">
-            <Link to="/forgot-password">Forgot Password?</Link>
-          </div>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        Need an account? <Link to="/signup">Sign Up</Link>
-      </div>
-    </>
-  );
+    const [formData, setFormData] = useState({
+        email: '', // required
+        password: '' // required
+    })
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        fetch('http://localhost:3000/login', {
+            method: 'POST',
+            headers: {'Content-Type' : 'application/json'},
+            body: JSON.stringify(formData)
+        })
+        .then(res => res.json())
+        .then(data => console.log(data.user))
+    }
+
+    function handleChange(e) {
+        setFormData({...formData, [e.target.name] : e.target.value})
+    }
+
+    return (
+        <div>
+            <h1>Login Form</h1>
+            <form className='login-form' onSubmit={e => handleSubmit(e)}>
+                <input type='text' placeholder='Email' value={formData.email} name='email' onChange={e => handleChange(e)} ></input>
+                <input type='text' placeholder='Password' value={formData.password} name='password' onChange={e => handleChange(e)} ></input>
+                <button className='login-btn' type='submit'>Login</button>
+            </form>
+        </div>
+    )
 }
+
+export default Login
