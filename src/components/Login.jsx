2c643a7ee4d5bc708beaff76cdc6078b7a03a914
mustafa <musmoh73@gmail.com>
2023-07-12 23:51:35 +0300

    add jeson add

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,52 +1,61 @@
-import { React, useRef, useState } from "react";
-import { useAuth } from "../context/AuthContext";
+import { React } from "react";
 import { Form, Button, Card, Alert } from "react-bootstrap";
-import { Link, useNavigate, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 export default function Login() {
-  const emailRef = useRef();
-  const passwordRef = useRef();
-  const { login } = useAuth();
-  const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
-  const location = useLocation();
-  const redirectPath = location.state?.path || "/";
-  async function handleSubmit(e) {
-    e.preventDefault();
-
-    try {
-      setError("");
-      setLoading(true);
-      await login(emailRef.current.value, passwordRef.current.value);
-      navigate(redirectPath, { replace: true });
-    } catch {
-      setError("Failed to log in");
-    }
-
-    setLoading(false);
-  }
-
+   
+   
+    
+    
+    const [error, setError] = useState("");
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+      
+      try {
+        // Make a POST request to the JSON server with the login credentials
+        const response = await axios.post("http://localhost:3000/login", {
+          email,
+          password
+        });
+        
+        // Handle the response based on the server's authentication logic
+        if (response.data.success) {
+          // Handle successful login
+        } else {
+          setError(response.data.message);
+        }
+      } catch (error) {
+        console.log(error);
+        setError("An error occurred while logging in.");
+      }
+    };
+    
+  
+  
   return (
     <>
       <Card>
         <Card.Body>
           <h2 className="text-center mb-4">Log In</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group>
               <Form.Label htmlFor="email">Email</Form.Label>
-              <Form.Control id="email" type="text" ref={emailRef} required />
+              <Form.Control id="email" type="text" required />
             </Form.Group>
             <Form.Group>
               <Form.Label htmlFor="password">Password</Form.Label>
               <Form.Control
                 id="password"
                 type="password"
-                ref={passwordRef}
+              
                 required
               />
             </Form.Group>
-            <Button disabled={loading} className="w-100 mt-3" type="submit">
+            <Button className="w-100 mt-3" type="submit">
               Log In
             </Button>
           </Form>
