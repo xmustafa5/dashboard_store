2bb58a7c8014ba8ede7e2290d285314445108f08
mustafa <musmoh73@gmail.com>
2023-07-13 23:58:34 +0300

    add layout to app

--- src/App.js
+++ src/App.js
@@ -9,12 +9,15 @@ import AuthProvider from './context/AuthContext';
 import RequireAuth from './context/RequirAuth';
 // import {Container} from "react-bootstrap"
 import Items from './components/Items'; // Add this line
+import Layout from './layout/Index';
   
 const App =() =>{
   return (
      <Container className='d-flex align-items-center justify-content-center ' style={{minHeight:"100vh"}}>
 
 <div className='w-100' style={{maxWidth:"400px"}}>
+<Layout>
+
 <AuthProvider>
   <Routes>
           <Route path='/Singup' element={<Singup />} />
@@ -26,6 +29,7 @@ const App =() =>{
         </Routes> 
         </AuthProvider>
 </div>
+ </Layout>   
     </Container>
   );
 }
